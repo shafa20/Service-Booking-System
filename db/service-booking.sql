a7e6783055bf837b4404a68d935f26ad79c91033
shafa20 <shafakhan2018@gmail.com>
2025-07-30 00:23:33 +0600

    latest db and collection added

--- db/service-booking.sql
+++ db/service-booking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 29, 2025 at 08:29 AM
+-- Generation Time: Jul 29, 2025 at 08:21 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.3.23
 
@@ -36,6 +36,14 @@ CREATE TABLE `bookings` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`id`, `user_id`, `service_id`, `booking_date`, `status`, `created_at`, `updated_at`) VALUES
+(1, 2, 1, '2025-07-30', 'pending', '2025-07-29 11:10:55', '2025-07-29 11:10:55'),
+(2, 1, 3, '2025-07-30', 'pending', '2025-07-29 12:08:05', '2025-07-29 12:08:05');
 
 -- --------------------------------------------------------
 
@@ -133,7 +141,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '0001_01_01_000001_create_cache_table', 1),
 (3, '0001_01_01_000002_create_jobs_table', 1),
 (4, '2025_07_29_000003_create_services_table', 1),
-(5, '2025_07_29_000004_create_bookings_table', 1);
+(5, '2025_07_29_000004_create_bookings_table', 1),
+(6, '2025_07_29_090734_create_personal_access_tokens_table', 1);
 
 -- --------------------------------------------------------
 
@@ -146,6 +155,35 @@ CREATE TABLE `password_reset_tokens` (
   `token` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` text NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `personal_access_tokens`
+--
+
+INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `name`, `token`, `abilities`, `last_used_at`, `expires_at`, `created_at`, `updated_at`) VALUES
+(1, 'App\\Models\\User', 2, 'MyApp', 'c73c448bc5dc176cc3b51b641498d846255b8f08eb9e04efaacaea1bbc955367', '[\"*\"]', NULL, NULL, '2025-07-29 11:00:13', '2025-07-29 11:00:13'),
+(2, 'App\\Models\\User', 1, 'MyApp', '7cbeeada5b9122baaf5485dee8af0f9d7e4aff3cbcbd86e5cceaa8626ad29ed1', '[\"*\"]', NULL, NULL, '2025-07-29 11:01:56', '2025-07-29 11:01:56'),
+(3, 'App\\Models\\User', 2, 'MyApp', '35173ae0e5c9476cfac70780c24b2330c856ce2b2fce2dfee03ab8b4caf3445c', '[\"*\"]', '2025-07-29 11:17:39', NULL, '2025-07-29 11:04:07', '2025-07-29 11:17:39'),
+(4, 'App\\Models\\User', 2, 'MyApp', '1bd90e2b2989651a31b010181ed787a174b811d5ad6222db905adb93dd9d0669', '[\"*\"]', '2025-07-29 11:22:35', NULL, '2025-07-29 11:22:00', '2025-07-29 11:22:35');
 
 -- --------------------------------------------------------
 
@@ -168,9 +206,10 @@ CREATE TABLE `services` (
 --
 
 INSERT INTO `services` (`id`, `name`, `description`, `price`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'AC Repair', 'Air conditioner repair and maintenance', 1500.00, 'active', '2025-07-29 00:16:18', '2025-07-29 00:16:18'),
-(2, 'Plumbing', 'Plumbing services for home and office', 1000.00, 'active', '2025-07-29 00:16:18', '2025-07-29 00:16:18'),
-(3, 'Electrical', 'Electrical wiring and repair', 1200.00, 'active', '2025-07-29 00:16:18', '2025-07-29 00:16:18');
+(1, 'Hair Cut Deluxe', 'Deluxe haircut service', 150.00, 'active', '2025-07-29 10:57:36', '2025-07-29 12:08:38'),
+(2, 'Plumbing', 'Plumbing services for home and office', 1000.00, 'active', '2025-07-29 10:57:36', '2025-07-29 10:57:36'),
+(3, 'Electrical', 'Electrical wiring and repair', 1200.00, 'active', '2025-07-29 10:57:36', '2025-07-29 10:57:36'),
+(4, 'Hair Cut', 'Professional haircut service', 100.00, 'active', '2025-07-29 11:23:54', '2025-07-29 11:23:54');
 
 -- --------------------------------------------------------
 
@@ -210,7 +249,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `role`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Admin User', 'admin@admin.com', 'admin', '2025-07-29 00:16:17', '$2y$12$DvJqkg.cF3BZHbJxqGWB..Ak9i6oOLmkkymKc4eAsiWRPc87haIUK', '31h7OYmL88', '2025-07-29 00:16:18', '2025-07-29 00:16:18');
+(1, 'Admin User', 'admin@admin.com', 'admin', '2025-07-29 10:57:35', '$2y$12$la3levJ7g6g9mPW5.hb.8.blQPaP6akdhkkhI7MsQhmsiZy8TjvKW', 'UIkwVVocZK', '2025-07-29 10:57:36', '2025-07-29 10:57:36'),
+(2, 'John Doe', 'john@example.com', 'customer', NULL, '$2y$12$meGl0qvKGKAKpLR3IWArbua8WVo/4P2udJVHMWZ/bE/pYYElHCRua', NULL, '2025-07-29 11:00:13', '2025-07-29 11:00:13');
 
 --
 -- Indexes for dumped tables
@@ -269,6 +309,15 @@ ALTER TABLE `password_reset_tokens`
   ADD PRIMARY KEY (`email`);
 
 --
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`),
+  ADD KEY `personal_access_tokens_expires_at_index` (`expires_at`);
+
+--
 -- Indexes for table `services`
 --
 ALTER TABLE `services`
@@ -297,7 +346,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -315,19 +364,25 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `services`
 --
 ALTER TABLE `services`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
